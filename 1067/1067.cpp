9884751706325d0cbf2c794ff4928c1cf87e9da0
pedrogattosch <pedrogattosch@gmail.com>
2025-07-15 16:15:54 -0300

    feat: solve exercise 1067

---
+++ 1067/1067.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        if(i % 2 == 1) cout << i << endl;
+    }
+
+    return 0;
+}
