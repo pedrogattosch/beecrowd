0693516385e055fd519683922fe55f4fbd10b753
pedrogattosch <pedrogattosch@gmail.com>
2025-07-17 10:53:03 -0300

    feat: solve exercise 1098

---
+++ 1098/1098.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    for(float i = 0; i < 2.01; i += 0.2){
+        for(int j = 1; j < 4; j++){
+            cout << "I=" << i << " J=" << j+i << endl;
+        }
+    }
+
+    return 0;
+}
