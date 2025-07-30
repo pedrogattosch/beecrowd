b4e24da705488e1209186bc16f0727ccace85a7a
pedrogattosch <pedrogattosch@gmail.com>
2025-07-30 15:43:12 -0300

    feat: solve exercise 1176

---
+++ 1176/1176.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int T;
+    cin >> T;
+
+    vector<long long> fib(61); 
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int i = 2; i <= 60; ++i){
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    while (T--){
+        int N;
+        cin >> N;
+        cout << "Fib(" << N << ") = " << fib[N] << endl;
+    }
+
+    return 0;
+}
