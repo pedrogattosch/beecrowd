9f4dea6622d4767fb2561dc54a14969520058f28
pedrogattosch <pedrogattosch@gmail.com>
2025-07-31 09:42:17 -0300

    feat: solve exercise 1184

---
+++ 1184/1184.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    char operation;
+    cin >> operation;
+
+    double matrix[12][12];
+    for(int i = 0; i < 12; ++i){
+        for(int j = 0; j < 12; ++j){
+            cin >> matrix[i][j];
+        }
+    }
+
+    double sum = 0.0;
+    int count = 0;
+
+    for(int i = 0; i < 12; ++i){
+        for(int j = 0; j < 12; ++j){
+            if(i > j){
+                sum += matrix[i][j];
+                count++;
+            }
+        }
+    }
+
+    if(operation == 'S'){
+        cout << fixed << setprecision(1);
+        cout << sum << endl;
+    } else if (operation == 'M'){
+        if(count > 0){ 
+            double average = sum / (double)count;
+            cout << fixed << setprecision(1);
+            cout << average << endl;
+        }
+    }
+
+
+    return 0;
+}
